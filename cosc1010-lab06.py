9de222d56e4bbf316d3e42eb1cbf2bd424d2d1ec
Wyomungandr <asharum90@gmail.com>
2024-10-16 00:20:22 +0000

    Finished

--- cosc1010-lab06.py
+++ cosc1010-lab06.py
@@ -64,11 +64,26 @@ zkdenxczyooloczcaahnkehbwimvieedpdlqfafbqvxvfmvabd
 random_string = random_string.replace("\n","") #remove all newline characters
 print(len(random_string)) # Print out the size for reference 
 
+char_count = {}
+
 # Above is a string with 2500 characters.
 # Create a program that goes through and counts the occurrence of each character, excluding \n using a  dictionary
+for char in random_string:
+    if char in char_count:
+        char_count[char] += 1
+    else:
+        char_count[char] = 1
+
 # Output each letter and its corresponding occurrence in alphabetical order
+ord_char_count = dict(sorted(char_count.items()))
+for char, count in ord_char_count.items():
+    print(f'{char}: {count}')
 # Output which letter occurred the most 
 # Output which letter occurred the least 
+most_occ = max(char_count, key=char_count.get)
+least_occ = min(char_count, key=char_count.get)
+print(f"The letter that occurred the most is '{most_occ}' with {char_count[most_occ]} occurrences.")
+print(f"The letter that occurred the least is '{least_occ}' with {char_count[least_occ]} occurrences.")
 # Output what the percentage of the string each character is, again in alphabetical
 
 #Tips and trick:
@@ -88,13 +103,17 @@ print(len(random_string)) # Print out the size for reference
 print("*"*75)
 # Output which letter occurred the most 
 
-most_occurred = ""
-least_occurred = ""
 
-print(f"The letter that occurred the most is {most_occurred}")
+
+print(f"The letter that occurred the most is {most_occ}")
 print("*"*75)
 # Output which letter occurred the least 
-print(f"The letter that occurred the most is {least_occurred}")
+print(f"The letter that occurred the most is {least_occ}")
 print("*"*75)
 
 # Output what the percentage of the string each character is, again in alphabetical
+total_chars = len(random_string)
+print("Percentage of each character in the string:")
+for char, count in ord_char_count.items():
+    percent = (count / total_chars) * 100
+    print(f"{char}: {percent:.2f}%")
