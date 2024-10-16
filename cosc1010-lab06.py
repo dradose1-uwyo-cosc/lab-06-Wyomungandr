44557562749f0345a0505d17256c622b6dbcfb4f
Wyomungandr <asharum90@gmail.com>
2024-10-16 00:23:01 +0000

    Done

--- cosc1010-lab06.py
+++ cosc1010-lab06.py
@@ -1,8 +1,8 @@
-# Your Name Here
+# Andrew Sharum
 # UWYO COSC 1010
-# Submission Date
+# 15OCT24
 # Lab 06
-# Lab Section: 
+# Lab Section: 11
 # Sources, people worked with, help given to: 
 # your
 # comments
